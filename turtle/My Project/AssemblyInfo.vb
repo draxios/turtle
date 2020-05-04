1750f32ac372fe4859410d51194c8058d220b48f
Scott Stevens <scott.axios@gmail.com>
2020-05-04 10:43:30 -0500

    Add files via upload

---
+++ turtle/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Turtle")> 
+<Assembly: AssemblyDescription("Turtle peeks it's head out and sees what is going on!")> 
+<Assembly: AssemblyCompany("EHI")> 
+<Assembly: AssemblyProduct("Turtle")> 
+<Assembly: AssemblyCopyright("Copyright © EHI 2014")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("b7ada34d-2067-42d4-95e0-63e46b38d9ed")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.2.0")> 
+<Assembly: AssemblyFileVersion("1.0.2.0")> 
